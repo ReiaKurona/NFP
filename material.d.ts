cc4bc068d542fd2b4527faaa8e9692de8461a0b5
ReiaKurona <z19906567940@gmail.com>
2026-03-05 10:10:35 +0800

    Create material.d.ts

---
+++ material.d.ts
@@ -0,0 +1,10 @@
+// material.d.ts
+declare namespace JSX {
+  interface IntrinsicElements {
+    'md-filled-button': any;
+    'md-icon-button': any;
+    'md-fab': any;
+    'md-ripple': any;
+    'md-icon': any;
+  }
+}
